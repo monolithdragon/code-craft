42f511c2e6b8f2548f1739fe9b88eece0afe2363
Joó Lajos <88922090+monolithdragon@users.noreply.github.com>
2025-03-09 20:58:57 +0100

    Auth and Database setup

---
+++ src/components/providers/convex-client/index.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { ClerkProvider, useAuth } from '@clerk/nextjs';
+import { ConvexReactClient } from 'convex/react';
+import { ConvexProviderWithClerk } from 'convex/react-clerk';
+import React from 'react';
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+type Props = {
+	children: React.ReactNode;
+};
+
+function ConvexClientProvider({ children }: Props) {
+	return (
+		<ClerkProvider
+			publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}>
+			<ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+				{children}
+			</ConvexProviderWithClerk>
+		</ClerkProvider>
+	);
+}
+
+export default ConvexClientProvider;
